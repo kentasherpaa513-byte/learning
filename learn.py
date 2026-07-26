5ee684a1f333e61aef2bd24584ab8716dccbbcc6
TheFool-8 <kentasherpaa513@gmail.com>
2026-07-26 16:38:51 +0545

    Guessing a number game

--- learn.py
+++ learn.py
@@ -1,11 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+#learning loops
+
+# for i in range(16,0,-1):
+#     print(i)
+
+# a = "Kenta Sherpa"
+# print(len(a))
+# for i in range(len(a)):
+#     if i==4:
+#         continue    
+#     else:
+#         print(a[i])
+# n = 5
+# # for i in range(n):
+# #     print(i)
+# n=int(input("Enter a number: "))
+# count = 0
+# sum = 0
+# for i in range(1,n+1):
+#     if n % i ==0:
+#         count+=1
+# if count == 2:
+#     print("is",n,"a prime number")
+# else:
+#     print(n,"is not a prime number")
+        
+import random
+
+
+
+for i in range(3):
+    x = random.randint(1, 10)
+    guess = int(input("Guess a number between 1 and 10: "))
+    if guess == x:
+        print("Congratulations! You guessed the correct number.")
+    else:
+        print("Sorry, that's not the correct number.The correct number was", x)
+
